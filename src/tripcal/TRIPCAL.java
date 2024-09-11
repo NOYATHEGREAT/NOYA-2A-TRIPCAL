45d8174706f89b87b920783e7cb1cd9d557fff5c
NOYATHEGREAT <johnlloydisguapo@gmail.com>
2024-09-11 11:37:37 +0800

    TRIPCAL

---
+++ src/tripcal/TRIPCAL.java
@@ -0,0 +1,81 @@
+
+package tripcal;
+
+import java.util.Scanner;
+
+public class TRIPCAL {
+    
+    private int daysOfStay;
+    private double dailyAccommodationCost;
+    private double dailyMealCost;
+    private double transportationCost;
+    private double discountRate;
+    
+    
+    public TRIPCAL(int daysOfStay, double dailyAccommodationCost, double dailyMealCost, double transportationCost, double discountRate) {
+        this.daysOfStay = daysOfStay;
+        this.dailyAccommodationCost = dailyAccommodationCost;
+        this.dailyMealCost = dailyMealCost;
+        this.transportationCost = transportationCost;
+        this.discountRate = discountRate;
+    }
+    
+    public double calculateTotalCostBeforeDiscount() {
+        double totalAccommodationCost = daysOfStay * dailyAccommodationCost;
+        double totalMealCost = daysOfStay * dailyMealCost;
+        return totalAccommodationCost + totalMealCost + transportationCost;
+    }
+    
+    public double calculateDiscountAmount(double totalCostBeforeDiscount) {
+        return (discountRate / 100) * totalCostBeforeDiscount;
+    }
+    
+   
+    public double calculateTotalCostAfterDiscount(double totalCostBeforeDiscount, double discountAmount) {
+        return totalCostBeforeDiscount - discountAmount;
+    }
+    
+    public int getDaysOfStay() {
+        return daysOfStay;
+    }
+
+    public double getDailyAccommodationCost() {
+        return dailyAccommodationCost;
+    }
+
+    public double getDailyMealCost() {
+        return dailyMealCost;
+    }
+
+    public double getTransportationCost() {
+        return transportationCost;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+}
+
+    
+
+        
+        
+    
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+     
+        
+        
+        
+     
+    
+    
+
